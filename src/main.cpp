11ff770aed481b01e1c7a1837f000f687f83fc79
slippedandmissed <gbt505@hotmail.com>
2023-07-31 13:43:09 +0100

    Compiler now takes file path as command line argument rather than input directly from stdin
    This should make import resolution easier

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,44 @@
 
 extern "C" ProgramNode *head;
 extern "C" int yyparse();
+extern "C" void yyrestart(FILE *);
 
-int main() {
+int main(int argc, char *argv[])
+{
+
+  if (argc < 2)
+  {
+    std::cerr << "Usage: " << (argc == 0 ? "compile" : argv[0]) << " [input_file.xlsl]" << std::endl;
+    exit(1);
+  }
+
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == nullptr)
+  {
+    std::cerr << "Failed to open file: " << argv[1] << std::endl;
+    exit(1);
+  }
+
+  yyrestart(fp);
   yyparse();
-  if (head == nullptr) {
+
+  fclose(fp);
+
+  if (head == nullptr)
+  {
     return 1;
   }
   ProgramNode *ast = head;
-  std::cout << "AST:" << std::endl << *ast << std::endl;
+  std::cout << "AST:" << std::endl
+            << *ast << std::endl;
   std::cout << std::endl;
 
   auto program = ConcreteTree::Program::fromAST(ast);
 
   AST::deallocTree(ast);
 
-  std::cout << "Concrete Tree:" << std::endl << *program << std::endl;
+  std::cout << "Concrete Tree:" << std::endl
+            << *program << std::endl;
   std::cout << std::endl;
 
   return 0;
